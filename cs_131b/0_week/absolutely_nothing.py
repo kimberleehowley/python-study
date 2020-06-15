0173bca8440d3b43a5e63fcc38c5a88b4267f1b4
Kimberlee Johnson <kimee.johnson@gmail.com>
2020-06-15 16:27:22 -0700

    Added file that is entirely comments, per the course instructions

---
+++ cs_131b/0_week/absolutely_nothing.py
@@ -0,0 +1,9 @@
+# This is a comment! 
+'''This isn't technically a comment, but nothing happens in between it!'''
+"""Same goes here!"""
+
+# I am so excited for everything we will learn in this course. 
+# 🐍🐍🐍🐍🐍🐍🐍🐍🐍🐍🐍
+# To any of my fellow peers reading this, have you heard of pyLadies? 
+# There's a Slack channel and a former meetup group in SF. 
+# It doesn't look like much is happening now, but study groups would be great! 
